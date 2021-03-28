abc8e482ee770af217e891b04df13a5a48452299
Your Name <1606297247@qq.com>
2021-03-28 22:39:00 +0800

    下拉评论功能实现

--- deal/urls.py
+++ deal/urls.py
@@ -2,5 +2,9 @@ from deal.views import *
 from django.urls import path
 
 urlpatterns = [
+    
     path('',itemList.as_view()),
+
+    path('<int:pk>',itemDetail.as_view()),
+
 ]
--- deal/views.py
+++ deal/views.py
@@ -6,6 +6,23 @@ from .models import *
 from search.common import commonSearch
 from django.contrib.contenttypes.models import ContentType
 from comment.models import Comment
+from comment.forms import CommentForm
+from django.http.response import JsonResponse
+
+class itemDetail(View):
+    def post(self,request,pk):
+        item = get_object_or_404(Item,pk = pk)
+        form = CommentForm(request.POST)
+        content_type = ContentType.objects.get_for_model(item)
+        if form.is_valid():
+            cmt = Comment()
+            cmt.user = request.user
+            cmt.text = form.cleaned_data['text']
+            cmt.object_id = pk
+            cmt.content_type = content_type
+            cmt.save()
+            # 保存评论
+        return JsonResponse({})        
 
 class itemList(View):
     def get(self,request):
