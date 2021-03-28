ce53b5f1f410618b84a692224b9f72b251b87ede
Your Name <1606297247@qq.com>
2021-03-28 19:07:36 +0800

    商品列表->下拉评论列表->前端

--- deal/views.py
+++ deal/views.py
@@ -4,6 +4,8 @@ from django.views.generic import *
 from django.shortcuts import *
 from .models import *
 from search.common import commonSearch
+from django.contrib.contenttypes.models import ContentType
+from comment.models import Comment
 
 class itemList(View):
     def get(self,request):
@@ -23,6 +25,13 @@ class itemList(View):
         if search:
             items = commonSearch(Item,'name',search)
             # 调用自定义搜索函数
+        for item in items:
+            content_type = ContentType.objects.get_for_model(item)
+            # 对象的类
+            comments = Comment.objects.filter(content_type = content_type,object_id = item.pk)
+            # 获取评论
+            item.comments = comments
+            # 添加属性
         context = {
             'items' : items,
             'chop' : chopable,
