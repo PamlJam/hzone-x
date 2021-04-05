2831e77990176b1c362fea553315a7169ce49da4
Your Name <1606297247@qq.com>
2021-04-05 22:22:58 +0800

    nothing really matters

--- crawler/schoolInfo.py
+++ crawler/schoolInfo.py
@@ -1,7 +1,10 @@
 import requests
 # 请求
+import re
+# 正则
 from lxml import etree
-# 解析文本
+# 解析
+
 def getInfos():
     infos = []
     # 返回结果
@@ -15,12 +18,14 @@ def getInfos():
     # 链接
     textList = data.xpath('//*[@id="bt"]/@title')
     # 标题
-    for i in range(15):
+    for i in range(len(linkList)):
         infos.append((
         # 生成二维数组
             'http://oa.hnu.cn:808/' + linkList[i],
             # 拼接网址
-            textList[i],
+            re.split('【|】',textList[i])[1],
+            re.split('【|】',textList[i])[2],
+            # 分割字符串
             data.xpath('//*[@id="ContentPlaceHolder1_TztgGridView_Label2_%d"]/text()' % i)[0],
             # 拼接路径
         ))
